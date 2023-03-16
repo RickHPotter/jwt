1b6afae93228fbc2ce0f0c69069c100dcd430e92
RickHPotter </SOPHiA16>
2023-03-16 20:33:57 -0300

    added comments

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -14,12 +14,8 @@ import (
 
 func Signup(c *gin.Context) {
 	// Get the email/pass off req body
-	var body struct {
-		Email    string
-		Password string
-	}
 
-	if c.Bind(&body) != nil {
+	if c.Bind(&(models.Body)) != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"Error": "Failed to read body.",
 		})
@@ -28,7 +24,7 @@ func Signup(c *gin.Context) {
 	}
 
 	// Hash the pass
-	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+	hash, err := bcrypt.GenerateFromPassword([]byte(models.Body.Password), 10)
 
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -38,7 +34,7 @@ func Signup(c *gin.Context) {
 	}
 
 	// Create the user
-	user := models.User{Email: body.Email, Password: string(hash)}
+	user := models.User{Email: models.Body.Email, Password: string(hash)}
 
 	result := initialisers.DB.Create(&user)
 
@@ -50,18 +46,14 @@ func Signup(c *gin.Context) {
 
 	// Respond
 	c.JSON(http.StatusAccepted, gin.H{
-		"Message": "Hell Yeah, Mate!",
+		"Message": "Hell Yeah, Mate! " + user.Email + " is signed up.",
 	})
 }
 
 func Login(c *gin.Context) {
 	// Get the email and pass off req body
-	var body struct {
-		Email    string
-		Password string
-	}
 
-	if c.Bind(&body) != nil {
+	if c.Bind(&(models.Body)) != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"Error": "Failed to read body.",
 		})
@@ -71,7 +63,7 @@ func Login(c *gin.Context) {
 	// Look up requested user
 	var user models.User
 
-	initialisers.DB.First(&user, "email = ?", body.Email)
+	initialisers.DB.First(&user, "email = ?", models.Body.Email)
 
 	if user.ID == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -81,7 +73,7 @@ func Login(c *gin.Context) {
 	}
 
 	// Compare sent in pass with saved user pass hash
-	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(models.Body.Password))
 
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -92,8 +84,8 @@ func Login(c *gin.Context) {
 
 	// Generate a JWT
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"subject": user.ID,
-		"exp":     time.Now().Add(time.Hour * 24 * 30).Unix(),
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
 	})
 
 	tokenStr, err := token.SignedString([]byte(os.Getenv("SECRET")))
@@ -105,19 +97,26 @@ func Login(c *gin.Context) {
 		return
 	}
 
-	// Send it back
-
-	// cookie
+	// Create a cookie
 	c.SetSameSite(http.SameSiteLaxMode)
 	c.SetCookie("Authorisation", tokenStr, 3600*24*30, "", "", false, true)
 
+	// Send JWT back
 	c.JSON(http.StatusAccepted, gin.H{
 		"Message": tokenStr,
 	})
 }
 
+// mock func just to see middleware working
 func Validate(c *gin.Context) {
-	user, _ := c.Get("user")
+
+	// this is only possible because there was a c.Set("user", user) in requireAuth
+	user, exists := c.Get("user")
+	if !exists {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"Error": "This is not supposed to happen, by the way.",
+		})
+	}
 
 	c.JSON(http.StatusOK, gin.H{
 		"Message": user.(models.User).Email + " is logged in!",
